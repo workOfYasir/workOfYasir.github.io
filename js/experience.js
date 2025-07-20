9af89646c73d6263ad45e59173d032691c2f9b75
Yasir Iqbal <58635184+workOfYasir@users.noreply.github.com>
2025-07-20 15:03:07 +0500

    Update experience.js

--- js/experience.js
+++ js/experience.js
@@ -1,1 +1,1 @@
-(function($){'use strict';$(window).on('load',function(){function e(t){return((new Date()-new Date(t))/(1000*60*60*24*365.25)).toFixed(1)}$('#experience').text(e("2020-10-01")+"+")});})(jQuery);
+(function($){'use strict';$(window).on('load',function(){function e(t){return((new Date()-new Date(t))/(1000*60*60*24*365.25)).toFixed(1)}$('#experience').text(e("2020-10-01"))});})(jQuery);
